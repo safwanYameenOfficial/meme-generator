4df0f51a2afd54ad5fdf1a001765ffcaa92470d0
safwanYameenOfficial <safwanyameenofficial@gmail.com>
2024-06-13 15:02:13 +0300

    Header component added and styled

---
+++ meme-generator/components/Header.jsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+        <div>
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Project</h4>
+        </header>
+        </div>
+    )
+}
