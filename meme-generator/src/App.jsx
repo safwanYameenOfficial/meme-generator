2b6a3aa04014928dfc635162e638be7f1bed616c
safwanYameenOfficial <safwanyameenofficial@gmail.com>
2024-06-13 15:08:24 +0300

    Added and styled the Meme component and form elements within

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from '../components/Header'
+import Meme from '../components/Meme'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div>
       <Header />
-      
+      <Meme />
     </div>
   )
 }
